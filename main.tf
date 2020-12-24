26be7c4b0522147ee288ab7d83751d11fd4be7e6
divyansh2016 <70529963+divyansh2016@users.noreply.github.com>
2020-12-24 11:52:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,5 +2,9 @@ resource "aws_instance" "jenkins_terra_inte" {
 
     ami = "ami-00ddb0e5626798373"
     instance_type = "t2.micro"
+    tags = {
+        
+        Name = "terr-jenkins"
+        }
 
 }
